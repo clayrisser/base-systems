fa158c7efc7d1d630d8c1cea87d01ddabea8d2ac
Jam Risser <jam@jamrizzi.com>
2018-04-04 09:49:50 -0400

    Added more controls

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { TextField } from 'material-ui';
 import autobind from 'autobind-decorator';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 
 @autobind
 export default class App extends Component {
-  state = { base16: '', base10: 100, base2: '' };
+  state = { base16: '', base10: 100, base2: '', hexColor: '#FF9900' };
 
   componentWillMount() {
     const { base10 } = this.state;
@@ -13,6 +21,32 @@ export default class App extends Component {
       base16: base10.toString(16),
       base2: base10.toString(2)
     });
+  }
+
+  getColor(color, base) {
+    const { hexColor } = this.state;
+    let base16 = '';
+    switch (color) {
+      case 'red':
+        base16 = hexColor.substr(1, 2);
+        break;
+      case 'green':
+        base16 = hexColor.substr(3, 2);
+        break;
+      case 'blue':
+        base16 = hexColor.substr(5, 2);
+        break;
+    }
+    const base10 = parseInt(base16, 16);
+    switch (base) {
+      case 'base2':
+        return base10.toString(2);
+      case 'base10':
+        return base10;
+      case 'base16':
+        return base16;
+    }
+    return '';
   }
 
   handleBase16Change(e, base16) {
@@ -34,29 +68,96 @@ export default class App extends Component {
     this.setState({ base16: base10.toString(16), base10, base2 });
   }
 
+  handleHexColorChange(e, hexColor) {
+    this.setState({ hexColor: hexColor.toUpperCase() });
+  }
+
+  renderHexColorTable() {
+    return (
+      <Table>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn />
+            <TableHeaderColumn>Red</TableHeaderColumn>
+            <TableHeaderColumn>Green</TableHeaderColumn>
+            <TableHeaderColumn>Blue</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn>Base 2 (binary)</TableHeaderColumn>
+            <TableRowColumn>{this.getColor('red', 'base2')}</TableRowColumn>
+            <TableRowColumn>{this.getColor('green', 'base2')}</TableRowColumn>
+            <TableRowColumn>{this.getColor('blue', 'base2')}</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableHeaderColumn>Base 10</TableHeaderColumn>
+            <TableRowColumn>{this.getColor('red', 'base10')}</TableRowColumn>
+            <TableRowColumn>{this.getColor('green', 'base10')}</TableRowColumn>
+            <TableRowColumn>{this.getColor('blue', 'base10')}</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableHeaderColumn>Base 16 (hexadecimal)</TableHeaderColumn>
+            <TableRowColumn>{this.getColor('red', 'base16')}</TableRowColumn>
+            <TableRowColumn>{this.getColor('green', 'base16')}</TableRowColumn>
+            <TableRowColumn>{this.getColor('blue', 'base16')}</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    );
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <div>
-          <TextField
-            name="base16"
-            floatingLabelText="Base 16 (hexadecimal)"
-            value={this.state.base16}
-            onChange={this.handleBase16Change}
-          />
-          <TextField
-            name="base10"
-            type="number"
-            floatingLabelText="Base 10"
-            value={this.state.base10}
-            onChange={this.handleBase10Change}
-          />
-          <TextField
-            name="base2"
-            floatingLabelText="Base 2 (binary)"
-            value={this.state.base2}
-            onChange={this.handleBase2Change}
-          />
+          <h1>Base Number</h1>
+          <hr />
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <TextField
+              name="base2"
+              floatingLabelText="Base 2 (binary)"
+              value={this.state.base2}
+              onChange={this.handleBase2Change}
+            />
+            <TextField
+              name="base10"
+              type="number"
+              floatingLabelText="Base 10"
+              value={this.state.base10}
+              onChange={this.handleBase10Change}
+            />
+            <TextField
+              name="base16"
+              floatingLabelText="Base 16 (hexadecimal)"
+              value={this.state.base16}
+              onChange={this.handleBase16Change}
+            />
+          </div>
+          <h1>Hex Colors</h1>
+          <hr />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'center'
+            }}
+          >
+            <TextField
+              name="hexColor"
+              floatingLabelText="Hex Color"
+              value={this.state.hexColor}
+              onChange={this.handleHexColorChange}
+            />
+            <div
+              style={{
+                width: '400px',
+                height: '40px',
+                backgroundColor: this.state.hexColor
+              }}
+            />
+          </div>
+          <div>{this.renderHexColorTable()}</div>
         </div>
       </MuiThemeProvider>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import './style.scss';
 import App from './app';
 
 // eslint-disable-next-line no-undef
